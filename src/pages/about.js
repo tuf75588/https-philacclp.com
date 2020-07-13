127d537630731d40ead20644b529b252cc754237
Andrew Davis <atd285@gmail.com>
2020-07-12 21:46:49 -0400

    some more ui addons

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,36 @@
 import React from 'react';
+
 import Layout from '../components/layout';
+import Hero from '../components/hero';
+import Introduction from '../components/home-intro';
+import Services from '../components/services';
 import Container from '../components/container';
 import {css} from '@emotion/core';
-function About() {
-  return (
-    <Layout>
-      <Container>this will be the about page</Container>
-    </Layout>
-  );
-}
 
-export default About;
+const Card = ({
+  backgroundColor = '#E75248',
+  title,
+  description,
+  link,
+  big = false,
+}) => (
+  <Link to={link} aria-label={`View ${title}`}>
+    hello there
+  </Link>
+);
+
+const IndexPage = () => (
+  <Layout>
+    <Container
+      css={css`
+        position: relative;
+        border-radius: 5px;
+        padding: 40px 80px 60px 80px;
+      `}
+    >
+      <Hero />
+    </Container>
+  </Layout>
+);
+
+export default IndexPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,7 @@ import Layout from '../components/layout';
 import Hero from '../components/hero';
 import Introduction from '../components/home-intro';
 import Services from '../components/services';
+import {fonts, rhythm} from '../lib/typography';
+import {bpMaxXS, bpMaxSM, bpMaxMD} from '../lib/breakpoints';
 import Container from '../components/container';
 import {css} from '@emotion/core';
-const IndexPage = () => (
-  <Layout>
-    <Container
-      css={css`
-        position: relative;
-        border-radius: 5px;
-        padding: 40px 80px 60px 80px;
-      `}
-    >
-      <Hero />
-    </Container>
-  </Layout>
-);
-
-export default IndexPage;
