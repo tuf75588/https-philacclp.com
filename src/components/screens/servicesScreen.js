d00f5eb6361b431b47c97364627df8f0778f4782
Andrew Davis <atd285@gmail.com>
2020-07-23 21:24:06 -0400

    Some more initial styles for services components

--- src/components/screens/servicesScreen.js
+++ src/components/screens/servicesScreen.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import {css} from '@emotion/core';
+import styled from '@emotion/styled';
+import theme from '../../../config/theme';
 import Container from '../container';
 function ServicesCard() {
   return (
     <div
-      css={css`
-        width: 200px;
-        height: 300px;
-        border: 1px solid black;
-        position: absolute;
-      `}
+      css={{
+        width: 320,
+        border: '1px solid black',
+        background: theme.colors.white,
+        borderRadius: 5,
+        padding: 30,
+        position: 'relative',
+        paddingBottom: 60,
+        margin: 20,
+        boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.1)',
+      }}
     >
-      card component
+      <h2 css={{marginTop: 0}}>Consultation</h2>
+      <div css={{width: '100%'}}>
+        <button
+          css={{
+            position: 'relative',
+            display: 'block',
+            width: '100%',
+            bottom: 0,
+            left: 0,
+            borderTopLeftRadius: '0',
+            borderTopRightRadius: '0',
+          }}
+        >
+          Find out more
+        </button>
+      </div>
     </div>
   );
 }
+
+ServicesCard = React.memo(ServicesCard);
 
 function ServicesScreen() {
   return (
