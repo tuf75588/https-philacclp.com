fe38fe7e847baffe780373629274eebb5c08d540
Andrew Davis <atd285@gmail.com>
2020-08-26 19:07:57 -0400

    fun with framer motion!

--- src/components/screens/servicesScreen.js
+++ src/components/screens/servicesScreen.js
@@ -4,11 +4,11 @@ import theme from '../../../config/theme';
 import Container from '../container';
 import {css} from '@emotion/core';
 import {bpMaxSM} from '../../lib/breakpoints';
-
+import {motion} from 'framer-motion';
 function ServicesScreen() {
   return (
     <Container>
-      <div
+      <motion.div
         css={css`
           margin: 0 auto;
           display: grid;
@@ -19,6 +19,8 @@ function ServicesScreen() {
             background: red;
           }
         `}
+        animate={{rotate: 360}}
+        transition={{duration: 2}}
       >
         <div className="item">1</div>
         <div className="item">2</div>
@@ -26,7 +28,7 @@ function ServicesScreen() {
         <div className="item">4</div>
         <div className="item">5</div>
         <div className="item">6</div>
-      </div>
+      </motion.div>
     </Container>
   );
 }
